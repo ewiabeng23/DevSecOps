fac55e661fabc86a9b13528d38a616819891045a
immortal71 <newaashish190@gmail.com>
2026-01-20 22:19:27 -0800

    Fixed SQL Injection Lab: Add missing space before AND keyword (#321)
    - Fixed SQL query in views.py: Added space before AND keyword
    - Fixed documentation in sql.html to match corrected SQL syntax
    - Resolves issue where 'admin'AND caused SQL syntax error
    - Now correctly generates: user='admin' AND password='...'
    - Allows documented payload (anything' OR '1'='1) to work properly

--- introduction/views.py
+++ introduction/views.py
@@ -154,7 +154,7 @@ def sql_lab(request):
 
             if login.objects.filter(user=name):
 
-                sql_query = "SELECT * FROM introduction_login WHERE user='"+name+"'AND password='"+password+"'"
+                sql_query = "SELECT * FROM introduction_login WHERE user='"+name+"' AND password='"+password+"'"
                 print(sql_query)
                 try:
                     print("\nin try\n")
